d8225219b554361213eea5af043a6a0ef29d1cac
root <root@git.axelmwd.com>
2018-03-26 12:03:31 +0300

    fixes

--- SampleApi/Controllers/ValuesController.cs
+++ SampleApi/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace SampleApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Hello", "World", "test" };
+            return new string[] { "Hello", "World1", "test" };
         }
 
         // GET api/values/5
